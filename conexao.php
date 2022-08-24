a3c422bef4d1e8121994047792a1b8457cad51db
developer-anderson <anderson.asnascimentto@gmail.com>
2022-08-23 22:00:56 -0300

    ajustes

--- conexao.php
+++ conexao.php
@@ -1,8 +1,15 @@
 <?php
-    define('HOST', '201.48.8.141');
-    define('USER', 'dcenge68_admin');
-    define('PASSWORD', 'A@nderson301165');
-    define('DB', 'dcenge68_goobra');
+define( 'DB', 'escunass_novo' );
+
+/** MySQL database username */
+define( 'USER', 'escunass_site' );
+
+/** MySQL database password */
+define( 'PASSWORD', 'A@nderson301165' );
+
+/** MySQL hostname */
+define( 'HOST', '108.179.192.20' );
+
 
     $db = new mysqli(HOST, USER, PASSWORD, DB) or die ('Erro ao conectar');
     $db->set_charset("utf8");
--- db_view.php
+++ db_view.php
@@ -1,5 +1,5 @@
 <?php
-require_once('db.php');
+require_once('conexao.php');
 session_start();
 $data_atual = date("Y-m-d");
 $time = date("H:i:s");
@@ -7,14 +7,51 @@ $resultado = array();
 $mes = date("m");
 $dia = date("d");
 $ano = date("Y");
+
 if (isset($_POST['new_material'])) {
-    $sql  = "insert into ob_material_didatico set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", arquivo = '".$_POST['arquivo']."'".", link_drive = '".$_POST['link_drive']."'".", fl_aprovado = '".$_POST['fl_aprovado']."'";
-    $db->query($sql);
+    $sql  = "insert into ob_material_didatico set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", link_drive = '".$_POST['link_drive']."'".", fl_aprovado = '".$_POST['fl_aprovado']."'";
+    
+    if(isset($_FILES["arquivo"]) and !empty($_FILES["arquivo"]['name']))
+    {
+            $sql .= ", arquivo = '".$_POST['arquivo']."'";
+            $db->query($sql);
+            $foto = $_FILES["arquivo"]["name"];
+            $dir = "./material_didatico/" . $foto;
+            if(file_exists($dir)) 
+            {
+                unlink($dir);
+            }
+            move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir);
+         
+    }
+    else{
+        $db->query($sql);
+    }
+   
     $resultado = array("Status" => true);
 
+
 } elseif (isset($_POST['edit_material'])) {
-    $sql  = "update ob_material_didatico set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", arquivo = '".$_POST['arquivo']."'".", link_drive = '".$_POST['link_drive']."'".", fl_aprovado = '".$_POST['fl_aprovado']."' where id_material_didatico = ".$_POST['id_material_didatico'];
-    $db->query($sql);
+    $sql  = "update ob_material_didatico set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", link_drive = '".$_POST['link_drive']."'".", fl_aprovado = '".$_POST['fl_aprovado']."'";
+    if(isset($_FILES["arquivo"]) and !empty($_FILES["arquivo"]['name']))
+    {
+        $sql .= ", arquivo = '".$_POST['arquivo']."'  where id_material_didatico =".$_POST['id_material_didatico'];
+        $db->query($sql);
+        $foto = $_FILES["arquivo"]["name"];
+        $dir = "./material_didatico/" . $foto;
+        if(file_exists($dir)) 
+        {
+            unlink($dir);
+        }
+        move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir);
+        $sql = " update ob_material_didatico set `arquivo` = '$foto'  where id_material_didatico =".$_POST['id_material_didatico'];
+        $db->query($sql);
+    }
+    else{
+        $sql .= " where id_material_didatico =".$_POST['id_material_didatico'];
+        $db->query($sql);
+    }
+   
     $resultado = array("Status" => true);
 } 
 elseif (isset($_POST['view_material'])) {
@@ -51,13 +88,48 @@ elseif (isset($_POST['delete_material'])) {
 
 elseif (isset($_POST['new_video'])) {
     $sql  = "insert into ob_video set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", arquivo = '".$_POST['arquivo']."'";
-    $db->query($sql);
-    $resultado = array("Status" => true);
+
+    if(isset($_FILES["arquivo"]) and !empty($_FILES["arquivo"]['name']))
+    {
+            $sql .= ", arquivo = '".$_POST['arquivo']."'";
+            $db->query($sql);
+            $foto = $_FILES["arquivo"]["name"];
+            $dir = "./material_didatico/" . $foto;
+            if(file_exists($dir)) 
+            {
+                unlink($dir);
+            }
+            move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir);
+         
+    }
+    else{
+        $db->query($sql);
+    }
+    $resultado = array("status" => true, "desc" => "Video adicionado com sucesso");
 } 
 elseif (isset($_POST['edit_video'])) {
-    $sql  = "update ob_video set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", arquivo = '".$_POST['arquivo']."'";
-    $db->query($sql);
-    $resultado = array("Status" => true);
+    $sql  = "update ob_video set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'";
+    if(isset($_FILES["arquivo"]) and !empty($_FILES["arquivo"]['name']))
+    {
+        $sql .= ", arquivo = '".$_POST['arquivo']."'  where id_video =".$_POST['id_video'];
+        $db->query($sql);
+        $foto = $_FILES["arquivo"]["name"];
+        $dir = "./material_didatico/" . $foto;
+        if(file_exists($dir)) 
+        {
+            unlink($dir);
+        }
+        move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir);
+        $sql = " update ob_video set `arquivo` = '$foto'  where id_video =".$_POST['id_video'];
+        $db->query($sql);
+      
+    }
+    else{
+        $sql .= " where id_video =".$_POST['id_video'];
+        $db->query($sql);
+    }
+   
+    $resultado = array("status" => true, "desc" => "Video editado com sucesso");
 } 
 elseif (isset($_POST['view_video'])) {
     $sql  = "select * from  ob_video where 1=1";
@@ -87,20 +159,53 @@ elseif (isset($_POST['delete_video'])) {
         $sql .= " and id_video = ".$_POST['id_video'];
     }
     $db->query($sql);
-    $resultado = array("Status" => true);
+    $resultado = array("status" => true, "desc" => "Video excluido com sucesso");
 } 
 
 //ob relato
 
 elseif (isset($_POST['new_relato'])) {
-    $sql  = "insert into ob_relato set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", arquivo = '".$_POST['arquivo']."'";
-    $db->query($sql);
-    $resultado = array("Status" => true);
+    $sql  = "insert into ob_relato set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'";
+    if(isset($_FILES["arquivo"]) and !empty($_FILES["arquivo"]['name']))
+    {
+            $sql .= ", arquivo = '".$_POST['arquivo']."'";
+            $db->query($sql);
+            $foto = $_FILES["arquivo"]["name"];
+            $dir = "./material_didatico/" . $foto;
+            if(file_exists($dir)) 
+            {
+                unlink($dir);
+            }
+            move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir);
+         
+    }
+    else{
+        $db->query($sql);
+    }
+    $resultado = array("status" => true, "desc" => "Relato adicionado com sucesso");
 } 
 elseif (isset($_POST['edit_relato'])) {
-    $sql  = "update ob_relato set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'".", arquivo = '".$_POST['arquivo']."'";
-    $db->query($sql);
-    $resultado = array("Status" => true);
+    $sql  = "update ob_relato set id_professor = ".$_POST['id_professor'].", titulo = '".$_POST['titulo']."'".", descricao = '".$_POST['descricao']."'";
+    if(isset($_FILES["arquivo"]) and !empty($_FILES["arquivo"]['name']))
+    {
+        $sql .= ", arquivo = '".$_POST['arquivo']."'  where id_relato =".$_POST['id_relato'];
+        $db->query($sql);
+        $foto = $_FILES["arquivo"]["name"];
+        $dir = "./material_didatico/" . $foto;
+        if(file_exists($dir)) 
+        {
+            unlink($dir);
+        }
+        move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir);
+        $sql = " update ob_video set `arquivo` = '$foto'  where id_relato =".$_POST['id_relato'];
+        $db->query($sql);
+      
+    }
+    else{
+        $sql .= " where id_relato =".$_POST['id_relato'];
+        $db->query($sql);
+    }
+    $resultado = array("status" => true, "desc" => "Relato editado com sucesso");
 } 
 elseif (isset($_POST['view_relato'])) {
     $sql  = "select * from  ob_relato where 1=1";
@@ -130,6 +235,6 @@ elseif (isset($_POST['delete_relato'])) {
         $sql .= " and id_relato = ".$_POST['id_relato'];
     }
     $db->query($sql);
-    $resultado = array("Status" => true);
+    $resultado = array("status" => true, "desc" => "Relato excluido com sucesso");
 } 
 echo json_encode($resultado);
